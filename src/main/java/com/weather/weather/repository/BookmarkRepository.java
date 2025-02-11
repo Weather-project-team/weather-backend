522208c717836746ef51d32a9a75d2e8487706e6
ha0goo <likethat0923@gmail.com>
2025-02-11 16:39:28 +0900

    DB connect

---
+++ src/main/java/com/weather/weather/repository/BookmarkRepository.java
@@ -0,0 +1,13 @@
+package com.weather.weather.repository;
+
+import com.weather.weather.entity.Bookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    List<Bookmark> findByUserId(Long userId);
+}
+
